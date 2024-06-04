3a3f25a4831db5a62e7253b8b027ef675838bf0a
sam-shervin <samshervin.sebastian@gmail.com>
2024-06-04 19:01:58 +0530

    Oauth2.0 done Title of yt vid is sent to search api to request spotify

--- public/content.js
+++
@@ -1,6 +0,0 @@
-// Fetch the title of the YouTube video
-const videoTitle = document.querySelector("h1.ytd-watch-metadata").innerText;
-console.log(videoTitle);
-console.log("Content script running...");
-// Send the title to the background script
-chrome.runtime.sendMessage({ title: videoTitle });
--- src/App.js
+++ src/App.js
@@ -5,47 +5,58 @@ import Button from "./components/button";
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
-const REDIRECT_URI = "http://localhost:3000";
+const REDIRECT_URI = "https://easylike.token";
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const scope = encodeURIComponent("user-library-modify");
 
 function App() {
     const [accessToken, setAccessToken] = useState("");
-    const [searchQuery, setSearchQuery] = useState("");
-    const [searchResults, setSearchResults] = useState([]);
+    const [searchResults, setSearchResults] = useState({});
+    /*eslint-disable */
 
-    /* 
-    
-    useEffect(() => {
-        // API Access Token
-        const authParameters = {
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-                // tells the server what kind of content we are sending
-                // application/x-www-form-urlencoded is the default content type for POST requests
-                // it is used to send data to the server as key-value pairs
-                // example : key1=value1&key2=value2
-                // see the body variable below
+    const handleLogin = () => {
+        chrome.identity.launchWebAuthFlow(
+            {
+                url: `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&redirect_uri=${chrome.identity
+                    .getRedirectURL()
+                    .slice(0, -1)}&scope=${scope}&response_type=token`,
+                interactive: true,
             },
+            (redirectUri) => {
+                console.log(redirectUri);
+                const url = new URL(redirectUri);
+                const accessToken = url.hash.match(
+                    /[#&]access_token=([^&]*)/
+                )[1];
+                setAccessToken(accessToken);
+            }
+        );
+    };
+  
+    useEffect(async () => {
+        const title = (
+            await chrome.tabs.query({ active: true, currentWindow: true })
+        )[0].title;
+        const encodedTitle = encodeURIComponent(title); // Encode the title to ensure URL safety
+
+        // API Access Token
+        const headers = {
+            Authorization: `Bearer ${accessToken}`,
         };
 
-        const body = `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`;
-
         axios
-            .post(
-                "https://accounts.spotify.com/api/token",
-                body,
-                authParameters
+            .get(
+                `https://api.spotify.com/v1/search?q=${encodedTitle}&type=track&limit=10`,
+                { headers }
             )
             .then((response) => {
-                setAccessToken(response.data.access_token);
-                console.log(response.data.access_token);
-                //we're getting the access token from the response and storing it in the AccessToken state(variable))
+                setSearchResults(response.data);
+                // Handle the response data here
             })
             .catch((error) => {
-                console.error("Error fetching access token", error);
+                console.error("Error fetching data from Spotify API:", error);
             });
-    }, []); // empty array means that this useEffect will only run once when the component mounts
+    }, [accessToken]); // empty array means that this useEffect will only run once when the component mounts
     // use effect is a hook that lets you perform side effects in function components
     // it runs after the component has rendered
     // it is used to fetch data, subscribe to events, or manipulate the DOM directly
@@ -59,25 +70,9 @@ function App() {
     // if we remove the array, the useEffect will run every time the component renders
     // if we return a function from the useEffect, that function will run when the component unmounts
 
-    */
-/* eslint-disable */  
-    useEffect(() => {
-        const url = window.location.href;
-        const regex = /#access_token=([^&]+)/;
-        const match = regex.exec(url);
-        if (match) {
-            setAccessToken(match[1]);
-            console.log(match);
-        }
-    }, []);
+    /* eslint-disable */
 
-    const handle = () => {
-        const url = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${scope}&response_type=token`;
-        console.log("hello");
-        console.log(window.location);
-        chrome.tabs.create({ url });
-    };
-/* eslint-disable */
+    
     return (
         <>
             <section className="border-red-700 rounded-2xl w-[500px] min-h-screen bg-gradient-to-br from-cyan-800 to-green-700 flex flex-col justify-center ">
@@ -86,7 +81,7 @@ function App() {
                         <section className="text-4xl text-white self-center mt-12 font-light">
                             Login to Spotify
                         </section>
-                        <Button onClick={handle} />
+                        <Button id="login" onClick={handleLogin} />
                     </>
                 ) : (
                     <>
